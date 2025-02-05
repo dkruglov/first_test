6fc4bbc4b691964b5dcb70a3ed22fa8a2c61627b
dkruglov <denim0007denim@gmail.com>
2025-02-05 16:48:48 +0400

    изменения значения num в script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //изменения для commit
 let num = 10
 let num2 = 10
+num = 20
+
 let sum = num2 + num
 console.log(sum)
 console.log("Изучаем связку Гит и Вебшторм")
-console.log("хрен найдешь на ютубе видос про интеграцию Гит и Вебшторм")
+console.log("Нашлась удобная примочка - Local History при нажатии правой кнопкой мышки на  код")
