4ea94f3d331f07c7f93351f8df88106f3c770d1c
dkruglov <denim0007denim@gmail.com>
2025-02-05 23:39:25 +0400

    Коммит 3 новый - чудил в файле script.js

--- script.js
+++ script.js
@@ -7,4 +7,5 @@ let sum = num2 + num
 console.table(sum)
 console.log(sum)
 console.log("Изучаем связку Гит и Вебшторм")
+console.log("Изучаем связку Гит и Вебшторм2")
 console.log("Нашлась удобная примочка - Local History при нажатии правой кнопкой мышки     на  код")
